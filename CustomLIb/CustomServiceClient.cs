0cbfdeaa6203735548c927383254a7cc074ffce1
Maxuel Aragones <maxaragones@gmail.com>
2019-03-27 14:53:45 -0400

    added customlib project and reference in Workflow.Console

---
+++ CustomLIb/CustomServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLib
+{
+    public static class CustomServiceClient
+    {
+        public static string GetData()
+        {
+            return "This data came from some service...";
+        }
+    }
+}
