4749024e78d79c07fe61a5b7106a45c442919287
Maxuel Aragones <maxuel.aragones@equivant.com>
2021-01-04 16:09:19 -0500

    Cleaned and simplified WorkflowBed

--- WorkflowBed/Program.cs
+++ WorkflowBed/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace WorkflowBed
 {
@@ -10,40 +9,31 @@ namespace WorkflowBed
         {
             Console.WriteLine("**********************************");
             Console.WriteLine("**********Workflow Bed************");
-
-            ////////////////////
-            //TODO: SET THE PATH OF THE WORKFLOW TO INVOKE. THIS WILL BE EITHER A XAML OR XOML FILE
-            //////////////////
-
-            //string workflowFilePath = @"..\..\..\WorkflowLib\Training\DESDemoWorkflow.xaml";
-            string workflowFilePath = @"..\..\..\WorkflowLib\Templates\DESWorfklow.xaml";
-
-            //******************************DES workflow parameters*******************************
-            var parameters = new Dictionary<string, string>()
-            {
-                  { "casenumber", "2013cf123" } 
-                  //{ "PWD", "sh0wcas3" }
-            };
-
-            //run des workflow
-            var result = WorkflowUtil.RunDESWorkflow(workflowFilePath, parameters);
-
-            //************************************************************************************
-
-            //****************************ShowCase workflow parameters****************************
-
-            //var parameters = new object[]
-            //{
-            //    "First Argument Value",
-            //    "Second Argument Value"
-            //};
-            //run showcase workflow
-            //WorkflowUtil.RunShowCaseWorkflow(workflowFilePath, "50-2015-CC-025987-XXXX-MB", parameters);
-
-            //************************************************************************************
+           
+            RunTestWorkflow();
 
             Console.WriteLine("**********THE END************");
             Console.ReadLine();
+        }
+
+        static void RunTestWorkflow()
+        {
+            //1. Set the path of the XAML workflow to invoke.
+
+            string workflowFilePath = @"..\..\..\WorkflowLib\Training\TestWorkflow.xaml";
+
+            //2. Optionally add any parameters required by the workflow
+
+            var exchangeParams = new Dictionary<string, string>()
+            {
+               { "NAME", "Max" }
+            };
+
+            //3. Call ExecWorkflow or ExecWorkflowInDES if connected to a DES database.
+
+            //var response = WorkflowUtil.ExecWorkflowInDES(workflowFilePath, exchangeParams);
+
+            WorkflowUtil.ExecWorkflow(workflowFilePath, exchangeParams);
         }
     }
 }
--- WorkflowBed/WorkflowUtil.cs
+++ WorkflowBed/WorkflowUtil.cs
@@ -1,6 +1,5 @@
 ﻿using Exchange.Contracts;
-using ShowCaseClassLib;
-using ShowCaseClassLib.AuthenticationService;
+using Exchange.ServerLib;
 using ShowCaseWorkflowLib;
 using System;
 using System.Activities;
@@ -19,28 +18,57 @@ namespace WorkflowBed
     {
         static ConsoleColor _ogColor = Console.ForegroundColor;
 
-        public static void RunShowCaseWorkflow(string workflowFilePath, string caseNumber, object[] parms)
+        /// <summary>
+        /// Executes a XAML workflow in the DES context. This requires a DES <connectionStrings> configured in the app.config.
+        /// </summary>
+        /// <param name="workflowPath"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static ExchangeResponse ExecWorkflowInDES(string workflowPath, Dictionary<string, string> parameters)
         {
-            Console.WriteLine("running ShowCase workflow...", Path.GetFileName(workflowFilePath));
+            if (!IsWFPathValid(workflowPath))
+                return null;
 
-            //TODO: SET CASE PASSED TO THE WORKFLOW. THIS IS REQURED BY ANY SHOWCASE WORKFLOW.
-            CCase testingCase = GetCaseObject(caseNumber);
+            //Create an ExchangeRequest that models the real DES web request
+            ExchangeRequest request = new ExchangeRequest()
+            {
+                ExchangeID = 9999,
+                RequestID = Guid.NewGuid().ToString(),
+                ExchangeName = "Hello DES",
+                ProcessingMode = ProcessingMode.NonRealTime, // required for test
+                                                             //TODO: 2. Add any parameter required by the workflow. These come from the url query string, segment or exchange setting.
+                ExchangeParameters = parameters
+            };
 
-            //This runs the workflow
-            RunWorkflow(workflowFilePath, parms, testingCase);
+            var wfMgr = new WorkflowManager();
+
+            wfMgr.LoadFromPath(workflowPath);
+
+            var ep = new Exchange.ServerLib.ExchangePoint(true, wfMgr);
+
+            Console.Write("\nExecuting workflow {0} in DES...", Path.GetFileName(workflowPath));
+
+            var response = ep.PerformAsyncDataExchange(request);
+            Console.WriteLine("Done!\n");
+
+            Console.WriteLine("DES Response:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n{0}\n", response.ToString());
+            Console.ForegroundColor = _ogColor;
+
+            return response;
         }
 
-        public static XmlDocument RunDESWorkflow(string workflowFilePath, Dictionary<string, string> parameters)
+        /// <summary>
+        /// Executes a XAML workflow as a stand-alone. Unlike ExecWorkflowInDES, this does not requires a DES <connectionStrings> configured.
+        /// </summary>
+        /// <param name="workflowPath"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static XmlDocument ExecWorkflow(string workflowPath, Dictionary<string, string> parameters)
         {
-            Console.WriteLine("*Running DES workflow {0}...", Path.GetFileName(workflowFilePath));
-
-            if (!File.Exists(workflowFilePath))
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Workflow file not found!  :-(");
-                Console.ForegroundColor = _ogColor;
+            if (!IsWFPathValid(workflowPath))
                 return null;
-            }
 
             var exchangeRequest = new ExchangeRequest()
             {
@@ -53,7 +81,7 @@ namespace WorkflowBed
             dict.Add("ExchangeRequest", exchangeRequest);
 
             string workflowDef;
-            using (var xmlFile = XmlReader.Create(workflowFilePath))
+            using (var xmlFile = XmlReader.Create(workflowPath))
             {
                 StringBuilder sb = new StringBuilder();
                 while (xmlFile.Read())
@@ -63,33 +91,44 @@ namespace WorkflowBed
             }
 
             ActivityXamlServicesSettings settings = new ActivityXamlServicesSettings() { CompileExpressions = true };
-            using (var xamlReader = new XamlXmlReader(workflowFilePath))
+            using (var xamlReader = new XamlXmlReader(workflowPath))
             {
                 using (var innerReader = ActivityXamlServices.CreateReader(xamlReader))
                 {
                     Activity activity = ActivityXamlServices.Load(innerReader, settings);
                     var wfManager = new WorkflowInvoker(activity);
+                    
+                    Console.Write("\nExecuting workflow {0}...", Path.GetFileName(workflowPath));
 
                     var wfresult = wfManager.Invoke(dict);
                     XmlDocument xmlResult = null;
 
-                    Console.WriteLine("*Success :-)");
+                    Console.WriteLine("Done!\n");
 
                     if (wfresult != null && wfresult.ContainsKey("Result"))
                     {
-                        Console.WriteLine("*Result:");
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-
+                        Console.WriteLine("Workflow Response:\n");
+                        Console.ForegroundColor = ConsoleColor.Green;
                         xmlResult = (XmlDocument)wfresult["Result"];
-
                         Console.WriteLine(xmlResult?.OuterXml + "\n");
-
                         Console.ForegroundColor = _ogColor;
                     }
 
                     return xmlResult;
                 }      
             }
+        }
+
+        private static bool IsWFPathValid(string workflowPath)
+        {
+            if (!File.Exists(workflowPath))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Workflow file not found! :-(");
+                Console.ForegroundColor = _ogColor;
+                return false;
+            }
+            return true;
         }
 
         private static Dictionary<string, string> GetExchangeParameters(Dictionary<string, string> parameters)
@@ -101,149 +140,5 @@ namespace WorkflowBed
 
             return ep;
         }
-
-        /// <summary>
-        /// This method runs a workflow in the same way ShowCase does.
-        /// </summary>
-        /// <param name="workflowName"></param>
-        /// <param name="parms"></param>
-        static Dictionary<string, object> RunWorkflow(string workflowFilePath, object[] wfparms, CCase myCase)
-        {
-            bool isActivity = false;
-
-            string def = "";
-            string rules = "";
-            Dictionary<string, object> workflowResult = null;
-            XmlReader xmlFile = null; 
-
-            try
-            {
-                xmlFile = XmlReader.Create(workflowFilePath);
-                StringBuilder sb = new StringBuilder();
-                while (xmlFile.Read())
-                    sb.AppendLine(xmlFile.ReadOuterXml());
-
-                def = sb.ToString();
-
-                if (def.Trim().StartsWith("<Activity"))
-                    isActivity = true;
-                
-
-                ArrayList aParam = new ArrayList(wfparms);
-
-                if (isActivity)
-                {
-                    //This runs the XAML workflows
-                    var wfManager = new ActivityRuntimeManager(myCase);
-                    workflowResult  = wfManager.ExecuteXAMLActivity(def, aParam);
-                }
-                else
-                {
-                    //DEPRECATED WORKFLOWS
-                    var xomlParam = new Dictionary<string, object>();
-                    xomlParam.Add("Case", myCase);
-                    xomlParam.Add("ParameterValues", aParam);
-
-                    var rulesFileName = Path.GetDirectoryName(workflowFilePath) + "\\" + Path.GetFileNameWithoutExtension(workflowFilePath) + ".rules";
-
-                    if (File.Exists(rulesFileName))
-                    {
-                        xmlFile = XmlReader.Create(rulesFileName);
-                        sb = new StringBuilder();
-                        while (xmlFile.Read())
-                            sb.AppendLine(xmlFile.ReadOuterXml());
-                        rules = sb.ToString();
-                    }
-
-                    //This runs the XOML workflows
-                    var wfManager = new WorkflowManager();
-                    wfManager.ExecuteXOMLWorkflow(def, rules, xomlParam);
-
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error in {0} workflow. {1}", Path.GetFileName(workflowFilePath), ex.ToString());
-            }
-            finally
-            {
-                if (xmlFile != null)
-                    xmlFile.Dispose();
-            }
-
-            return workflowResult;
-        }
-
-        /// <summary>
-        /// This method runs the workflow allowing to debug it. 
-        /// </summary>
-        /// <param name="workflowName"></param>
-        /// <param name="parms"></param>
-        static void RunAndDebugWorkflow(string workflowName, Dictionary<string, object> parms)
-        {
-            //WorkflowInvoker.Invoke(workflowName);
-            var wfManager = new WorkflowManager();
-
-            string def = "";
-            string rules = "";
-
-            wfManager.ExecuteXOMLWorkflow(def, rules, parms);
-        }
-
-        static CCase GetCaseObject(string caseNumber)
-        {
-            CCase myCase;
-
-            if (!AuthenticateInShowCase())
-                return null;
-
-            try
-            {
-                int CaseID = 0;
-
-                myCase = ShowCaseDAL.CCaseHelper.GetShellCase(CGlobal.eShowCaseDLLUsage.Other);
-                myCase.GlobalClass().LoginID = ShowCaseDAL.CCaseHelper.GetLoginID();
-
-                CaseID = myCase.GetCaseIDByCaseNumber(caseNumber);
-
-                if (CaseID > 0)
-                    myCase.Load(CaseID, false, true, true);
-                else
-                    throw new Exception("Case '" + caseNumber + "' was not found");
-
-                return myCase;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error reading a Case object.\n{0}", ex.ToString());
-                return null;
-            }
-        }
-
-        static bool AuthenticateInShowCase()
-        {
-            try
-            {
-                string username = ConfigurationManager.AppSettings["UserName"];
-                string password = ConfigurationManager.AppSettings["Password"];
-
-                AuthenticationServiceClient authClient = new AuthenticationServiceClient("WSHttpBinding_IAuthenticationService");
-
-                Dictionary<string, string> additionalCredentials = new Dictionary<string, string>();
-                string sConn = ConfigurationManager.AppSettings["FriendlyDBName"];
-
-                
-
-                additionalCredentials.Add("friendlydbname", sConn);
-                Security.Authentication.AuthenticationResponse authenticationResponse = authClient.AuthenticateUser(username, password, additionalCredentials);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error authenticating in ShowCase.\n{0}", ex.ToString());
-                return false;
-            }
-        }
-
     }
 }
